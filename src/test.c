97f1ba6118bbe415fe4e6327166d7b0e474a6722
sho <me@shosato.jp>
2020-02-01 01:42:39 +0900

    test functions

--- src/test.c
+++ src/test.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,44 +17,88 @@ regex_state regex_match_simple(const char* pat, const char* flag, const char* st
     return result;
 }
 
-#define REGEX_TEST_BEGIN          \
-    int _regex_test_count_ok = 0, \
-        _regex_test_count_error = 0;
+void regex_test_fail(const char* pat, const char* flags, const char* str) {
+    if (regex_match_simple(pat, flags, str) != RS_FAILED) {
+        printf("ERROR: %s %s %s\n", pat, flags, str);
+        exit(0);
+    }
+}
+void regex_test(const char* pat, const char* flags, const char* str) {
+    if (regex_match_simple(pat, flags, str) == RS_FAILED) {
+        printf("ERROR: %s %s %s\n", pat, flags, str);
+        exit(0);
+    }
+}
 
-#define REGEX_TEST_FAIL(PAT, FLAG, STR)                            \
-    if (regex_match_simple((PAT), (FLAG), (STR)) != (RS_FAILED)) { \
-        _regex_test_count_error++;                                 \
-        printf(                                                    \
-            "ERROR(FAIL) : %s %s %s\n",                            \
-            (PAT), (FLAG), (STR));                                 \
-    } else {                                                       \
-        _regex_test_count_ok++;                                    \
-        printf(                                                    \
-            "OK : %s %s\n",                                        \
-            PAT, FLAG);                                            \
+#define REGEX_TEST(PAT, FLAGS, STR) \
+    regex_test((PAT), (FLAGS), (STR));
+#define REGEX_TEST_FAIL(PAT, FLAGS, STR) \
+    regex_test_fail((PAT), (FLAGS), (STR));
+
+bool regex_test_find_all(const char* pat, const char* flag, const char* str, int argc, const char** argv) {
+    regex* _regex = regex_compile(pat, flag);
+    regex_found f;
+    regex_found_init(&f);
+    regex_find_all(&str, _regex, &f);
+    bool result;
+    if (argc == f.results->length) {
+        array_each_i(f.results,
+                     if (!strcmp(array_ei, argv[array_i])) { result = true; } else {
+                         result = false;
+                         break;
+                     });
+    } else {
+        result = false;
     }
-#define REGEX_TEST(PAT, FLAG, STR)                                 \
-    if (regex_match_simple((PAT), (FLAG), (STR)) == (RS_FAILED)) { \
-        _regex_test_count_error++;                                 \
-        printf(                                                    \
-            "ERROR : %s %s %s\n",                                  \
-            (PAT), (FLAG), (STR));                                 \
-    } else {                                                       \
-        _regex_test_count_ok++;                                    \
-        printf(                                                    \
-            "OK : %s %s\n",                                        \
-            PAT, FLAG);                                            \
+    regex_destruct(_regex);
+    free(_regex);
+    return result;
+}
+
+bool regex_test_capture(const char* pat, const char* flag, const char* str, int argc, const char** argv) {
+    regex* _regex = regex_compile(pat, flag);
+    regex_options op;
+    regex_options_init(&op);
+    regex_state re = regex_match(&str, _regex, &op);
+
+    bool result;
+    if (re != RS_FAILED && argc == op.captured->length) {
+        array_each_i(op.captured,
+                     char* str = regex_capture_str(array_ei);
+                     if (!strcmp(str, argv[array_i])) { result = true; free(str); } else {
+                         result = false;
+                         free(str);
+                         break;
+                     });
+    } else {
+        result = false;
     }
-#define REGEX_TEST_END                                   \
-    printf("---------------------\n");                   \
-    if (_regex_test_count_error == 0) {                  \
-        printf("OK\n");                                  \
-    } else {                                             \
-        printf("ERROR : %d\n", _regex_test_count_error); \
+
+    regex_options_destruct(&op);
+    regex_destruct(_regex);
+    free(_regex);
+    return result;
+}
+
+#define REGEX_TEST_MULTI_ARGS(FN, PAT, FLAGS, STR, ...)         \
+    {                                                           \
+        const char* strs[] = {__VA_ARGS__};                           \
+        int len = sizeof(strs) / sizeof(void*);                 \
+        bool result = (FN)((PAT), (FLAGS), (STR), len, strs);   \
+        if (!result) {                                          \
+            printf("ERROR: %s %s %s\n", (PAT), (FLAGS), (STR)); \
+            exit(0);                                            \
+        }                                                       \
     }
+
+#define REGEX_TEST_FIND_ALL(PAT, FLAGS, STR, ...) \
+    REGEX_TEST_MULTI_ARGS(regex_test_find_all, PAT, FLAGS, STR, __VA_ARGS__)
+
+#define REGEX_TEST_CAPTURE(PAT, FLAGS, STR, ...) \
+    REGEX_TEST_MULTI_ARGS(regex_test_capture, PAT, FLAGS, STR, __VA_ARGS__)
+#define REGEX_TEST_END printf("OK\n");
 
 void test() {
-    REGEX_TEST_BEGIN
     REGEX_TEST("a", "", "a")
     REGEX_TEST("1", "", "1")
     REGEX_TEST_FAIL("1", "", "a")
@@ -72,97 +117,30 @@ void test() {
     REGEX_TEST_FAIL("\\d{3,5}", "", "34")
     REGEX_TEST("\\d{3,5}", "", "345")
 
-    REGEX_TEST("\\d+", "", "123")
     REGEX_TEST("(\\d+)", "", "123")
     REGEX_TEST("(\\d+)?", "", "")
     REGEX_TEST("(\\d+)?", "", "123")
-    REGEX_TEST_FAIL("^(\\d+)?", "", "a123")
-    REGEX_TEST_FAIL("(\\d+)?$", "", "123")
-    REGEX_TEST_END
+
+    REGEX_TEST("\\d+", "", "123")  // 5a
+    REGEX_TEST("\\d*", "", "a")    // 5a
+
+    REGEX_TEST("\\d*", "", "a")  // 5a
+
+    REGEX_TEST("(\\d+)", "", "1")
+    REGEX_TEST_FIND_ALL("\\d+", "", "1s2", "1", "2")
 }
-
-void dump_memory(void* __ptr, int __n) {
-    int width = 16;
-    printf("                ");
-    for (int i = 0; i < width; i++) printf("%2d ", i);
-    printf("\n---------------");
-    for (int i = 0; i < width; i++) printf("---");
-    printf("\n");
-    char* string = (char*)calloc(sizeof(char), width);
-    int count = 0;
-    while (count < __n) {
-        void* fp = __ptr;
-        printf("%p  ", fp);
-        for (int i = 0; i < width; i++) {
-            printf("%02x ", *((unsigned char*)__ptr + i));
-        }
-        for (int i = 0; i < width; i++) {
-            char c = *((unsigned char*)__ptr + i);
-            sprintf(string + (count % width), "%c", (31 < c && c < 126) ? c : '.');
-        }
-        __ptr = (char*)__ptr + width;
-        count += width;
-        printf(string);
-        memset(string, 0, width);
-        printf("\n");
-    }
-}
-
-// void dump_memory(void* __ptr, int __n) {
-//     int width = 16;
-
-//     printf("                ");
-//     for (int i = 0; i < width; i++) printf("%2d ", i);
-//     printf("\n---------------");
-//     for (int i = 0; i < width; i++) printf("---");
-//     printf("\n");
-
-//     char* memory = (char*)calloc(sizeof(char), width * 3);
-//     char* string = (char*)calloc(sizeof(char), width);
-//     int count = 0;
-//     char* fp = __ptr;
-//     while (count++ < __n) {
-//         int i = count % width;
-//         sprintf(memory + i, "%02x", *((unsigned char*)__ptr + i));
-//         char c = *((unsigned char*)__ptr + i);
-//         sprintf(string + i, "%c", (31 < c && c < 126) ?: '.');
-//         if (count && (i == 0)) {
-//             printf("%p  %s %s\n", fp, memory, string);
-//             memset(memory, 0, width * 3);
-//             memset(string, 0, width);
-//             fp += width;
-//         }
-//     }
-//     if (count % width)
-//         printf("%p  %s %s\n", fp, memory, string);
-
-//     free(memory);
-//     free(string);
-// }
 
 int main() {
-    {
-        // 実体
-        array* _array = array_new(sizeof(int), false, 1);
-        int a = 1;
-        array_push(_array, &a);
-        a = 2;
-        array_push(_array, &a);
-        a = 3;
-        array_push(_array, &a);
-        array_del(_array, 1);
-        array_each_i(_array, printf("%d\n", *(int*)array_ei));
-    }
-    {
-        // ポインタ
-        array* _array = array_new(sizeof(int*), true, 1);
-        int a = 1;
-        array_push(_array, &a);
-        int b = 2;
-        array_push(_array, &b);
-        int c = 3;
-        array_push(_array, &c);
-        array_del(_array, 1);
-        array_each_i(_array, printf("%d\n", *(int*)array_ei));
-    }
+    // test();
+    REGEX_TEST_FIND_ALL("([\\w\\.\\-]+)@([\\w\\.\\-]+)", "", "<a href=\"mailto:hoge@example.com\">Send Mail</a>", "hoge@example.com")
+    REGEX_TEST_FIND_ALL("0x[\\dA-F]{2}", "", "hoge0x340x12 hige0x5678", "0x34", "0x12", "0x56")
+    REGEX_TEST_FIND_ALL("<.*>", "i", "<H1>hoge</h1>", "<H1>hoge</h1>")
+    REGEX_TEST_FIND_ALL("<.*?>", "i", "<H1>hoge</h1>", "<H1>", "</h1>")
+
+    REGEX_TEST_FIND_ALL("Apple?", "i", "apple Appl APPLEE", "apple", "Appl", "APPLE")
+
+    REGEX_TEST_CAPTURE("(\\d+)", "", "222,234", "222")
+    REGEX_TEST_CAPTURE("(.*)@(.*)", "", "hoge@example.com", "hoge", "example.com")
+    REGEX_TEST_CAPTURE("(.*)@(?:.*)", "", "hoge@example.com", "hoge")
+    REGEX_TEST_END
 }
