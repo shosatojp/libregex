5d9b5ddf0a98ae1a4575255adba9d179c433e3b7
sho <me@shosato.jp>
2020-02-04 10:29:16 +0900

    整理

--- src/hashmap.c
+++ src/hashmap.c
@@ -48,11 +48,6 @@ hashmap* hashmap_new(int capacity) {
     return _m;
 }
 
-hash_entry* hash_entry_new() {
-    hash_entry* _e = (hash_entry*)calloc(sizeof(hash_entry), 1);
-    return _e;
-}
-
 int hashmap_init(hashmap* _m, int capacity) {
     _m->hash_entries = array_new(sizeof(hash_entry*), true, capacity);
     array_expand(_m->hash_entries, capacity);
@@ -91,8 +86,8 @@ int _hashmap_add(hashmap* _m, hash_entry* _e) {
         _m->length++;
         array_set(_m->hash_entries, _e, hash);
     } else {
-        hashtype hash2 = sdbm(_e->key) % _m->capacity;
-        hashtype count = 0;
+        hashtype hash2 = sdbm(_e->key) % _m->capacity,
+                 count = 0;
         while (true) {
             hash = (hash + hash2) % _m->capacity;
             if (!array_at(_m->hash_entries, hash)) {
@@ -100,9 +95,7 @@ int _hashmap_add(hashmap* _m, hash_entry* _e) {
                 array_set(_m->hash_entries, _e, hash);
                 return 0;
             }
-            if (count++ >= _m->capacity) {
-                return -1;
-            }
+            if (count++ >= _m->capacity) return -1;
         }
     }
 }
@@ -130,31 +123,28 @@ hash_entry* _hashmap_find(hashmap* _m, const char* key, hashtype* index) {
             }
             count++;
             hash = (hash + hash2) % _m->capacity;
-        } else {
+        } else
             break;
-        }
     }
     return NULL;
 }
 
 int hashmap_del(hashmap* _m, const char* key) {
-    hashtype index;
-    _hashmap_find(_m, key, &index);
-    hash_entry* entry = array_at(_m->hash_entries, index);
-    array_fill(_m->hash_entries, index, index, 0);
-    hash_entry_destruct(entry);
-    _m->length--;
+    hashtype index = 0;
+    hash_entry* entry = _hashmap_find(_m, key, &index);
+    if (entry) {
+        array_fill(_m->hash_entries, index, index, 0);
+        hash_entry_destruct(entry);
+        _m->length--;
+    }
 }
 
 int _hashmap_rehash(hashmap* _m, int capacity) {
     debug("rehash %d\n", capacity);
     hashmap* _tmp = hashmap_new(capacity);
-    hashmap_init(_tmp, capacity);
     for (hashtype i = 0; i < _m->capacity; ++i) {
         hash_entry* entry = array_at(_m->hash_entries, i);
-        if (entry) {
-            _hashmap_add(_tmp, entry);
-        }
+        if (entry) _hashmap_add(_tmp, entry);
     }
 
     // copy _tmp to _m
@@ -171,6 +161,11 @@ int hashmap_destruct(hashmap* _m) {
                      hash_entry_destruct(array_ei));
         array_clear(_m->hash_entries);
     }
+}
+
+hash_entry* hash_entry_new() {
+    hash_entry* _e = (hash_entry*)calloc(sizeof(hash_entry), 1);
+    return _e;
 }
 
 int hash_entry_destruct(hash_entry* _e) {
--- src/hashmaptest.c
+++ src/hashmaptest.c
@@ -5,21 +5,23 @@ int main() {
     hashmap* fruits = hashmap_new(2);
 
     /* insert key-value pair */
-    int a = 1;
+    int a = 5;
     hashmap_add(fruits, "apple", &a);
     hashmap_add(fruits, "apple", &a);
     int b = 2;
     hashmap_add(fruits, "orange", &b);
     int c = 3;
     hashmap_add(fruits, "melon", &c);
-
-    /* rehash */
-    // _hashmap_rehash(fruits, 10);
-    // _hashmap_rehash(fruits, 100);
-
-    /* retry inserting after rehash */
     int d = 4;
     hashmap_add(fruits, "banana", &d);
+
+    /* delete key */
+    hashmap_del(fruits, "banana");
+    hashmap_del(fruits, "hoge");
+
+    /* check if contains */
+    printf("orange : %d\n", hashmap_contains(fruits, "orange"));
+    printf("grapes : %d\n", hashmap_contains(fruits, "grapes"));
 
     /* find */
     int* e = hashmap_find(fruits, "apple");
@@ -29,14 +31,13 @@ int main() {
         printf("not found\n");
     }
 
-    printf("--------------\n");
-
     /* iterate map */
     hashmap_each_i(fruits, {
         hash_entry* _entry = hashmap_ei;
-        printf("%s : %d\n", _entry->key, *(int*)_entry->ptr);
+        printf("%10s : %d\n", _entry->key, *(int*)_entry->ptr);
     });
 
+    /* finalize */
     hashmap_destruct(fruits);
     free(fruits);
 }
